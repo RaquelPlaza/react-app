28817e7083d7deef68f755cfa6ac091b0d9b4e0f
unknown <Raquel.Plaza@PC-20177.zion.local>
2016-04-20 15:04:57 +0100

    initial commit.

---
+++ src/components/home/TilesComponent.js
@@ -0,0 +1,23 @@
+'use strict';
+
+import React from 'react';
+
+require('styles/home/Tiles.sass');
+
+class TilesComponent extends React.Component {
+  render() {
+    return (
+      <div className="tiles-component">
+        Please edit src/components/home//TilesComponent.js to update this component!
+      </div>
+    );
+  }
+}
+
+TilesComponent.displayName = 'HomeTilesComponent';
+
+// Uncomment properties you need
+// TilesComponent.propTypes = {};
+// TilesComponent.defaultProps = {};
+
+export default TilesComponent;
---
+++ src/components/new/ComponentComponent.js
@@ -0,0 +1,23 @@
+'use strict';
+
+import React from 'react';
+
+require('styles/new/Component.sass');
+
+class ComponentComponent extends React.Component {
+  render() {
+    return (
+      <div className="component-component">
+        Please edit src/components/new//ComponentComponent.js to update this component!
+      </div>
+    );
+  }
+}
+
+ComponentComponent.displayName = 'NewComponentComponent';
+
+// Uncomment properties you need
+// ComponentComponent.propTypes = {};
+// ComponentComponent.defaultProps = {};
+
+export default ComponentComponent;
